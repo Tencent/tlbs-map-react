123d042f14d29d470c22c652cab625d6164c772d
egbertshen <egbertshen@tencent.com>
2024-11-05 14:20:47 +0800

    chore: 新增快捷创建组件\示例\文档命令

---
+++ scripts/new-component/index.mjs
@@ -0,0 +1,61 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
+import { getComponentCode, getDemoCode, getDocCode } from './templates/index.mjs';
+
+const CWD = process.cwd();
+
+inquirer.prompt([{
+  type: 'input',
+  message: '请填写组件英文名称（需要使用大驼峰命名，例如MultiMarker）',
+  name: 'name',
+}, {
+  type: 'input',
+  message: '请填写组件中文名称（例如点标记）',
+  name: 'zhName',
+}]).then((answers) => {
+  let { name, zhName } = answers;
+  name = getFirstLetterUpper(name);
+  const dirPath = path.join(CWD, `./src/${name}`);
+  if (fs.existsSync(dirPath)) {
+    console.log(`${dirPath}已存在，请重新创建`);
+    return;
+  }
+  fs.mkdirSync(dirPath);
+  generateFile(`${dirPath}/index.tsx`, getComponentCode(name));
+  insertComponentToIndex(name, `${path.join(CWD, `./src/index.ts`)}`);
+  generateFile(`${dirPath}/demo.tsx`, getDemoCode(name));
+  generateFile(`${dirPath}/index.md`, getDocCode(name, zhName));
+});
+
+
+const generateFile = (filePath, content) => {
+  if (fs.existsSync(filePath)) {
+    console.log(`${filePath}已存在，请重新创建`);
+    return;
+  }
+  try {
+    fs.writeFileSync(filePath, content);
+  } catch (err) {
+    console.error('创建文件时出错:', err);
+  }
+};
+
+function insertComponentToIndex(name, indexPath) {
+  let data = fs.readFileSync(indexPath).toString();
+  data += `${getExportStr(name)}`;
+  fs.writeFile(indexPath, data, (err) => {
+    if (err) {
+      console.log(err, 'error');
+    }
+  });
+}
+
+function getExportStr(name) {
+  const firstLetterUpperName = getFirstLetterUpper(name);
+  return `\nexport {default as ${firstLetterUpperName}} from './${firstLetterUpperName}';`;
+}
+
+function getFirstLetterUpper(a) {
+  return a[0].toUpperCase() + a.slice(1);
+}
---
+++ scripts/new-component/templates/index.mjs
@@ -0,0 +1,86 @@
+export const getComponentCode = name => `
+import React, { useContext, useEffect, useImperativeHandle, useState, type FC } from 'react';
+import { MapContext } from 'tlbs-map-react';
+
+interface Props {
+
+}
+
+const ${name}Component: FC<Props> = React.forwardRef((props, ref) => {
+  const map = useContext(MapContext); // 获取地图实例
+  const [instance, setInstance] = useState<TMap.visualization.Heat>(); // 存储热力图图层实例
+
+  /** 初始化图层 */
+  const init${name} = () => {
+    if (!map) return;
+    // 图层初始化代码
+
+  };
+
+  // @hook 初始化图层
+  useEffect(() => {
+    if (!instance) init${name}();
+
+    return () => {
+      // 图层销毁
+    };
+  }, [map, instance]);
+
+  return null;
+});
+
+export default ${name}Component;
+`;
+
+export const getDocCode = (name, zhName) => `
+# ${zhName} ${name}
+
+**1. 组件示例**
+
+<code src="./demo.tsx"></code>
+:::info{title=提示}
+点击地图下方最右侧图标 \`</>\` 可展开代码
+:::
+
+**2. 组件 Props**
+
+
+`;
+
+export const getDemoCode = (name, zhName) => `
+import React, { useCallback, useRef, useState } from 'react';
+import { ${name}, BaseMap } from 'tlbs-map-react';
+import { Button } from 'tdesign-react';
+
+export default () => {
+  const layerRef: any = useRef(null); // 图层实例
+
+  /** 打印图层实例 */
+  const printInstance = useCallback(() => {
+    console.log('🚀🚀🚀  打印图层实例', layerRef.current);
+  }, []);
+
+  return (
+    <div className='demo-box'>
+      <div className="action-box">
+        <Button type="button" onClick={printInstance}>
+          打印图层实例
+        </Button>
+      </div>
+      <BaseMap
+        apiKey="OB4BZ-D4W3U-B7VVO-4PJWW-6TKDJ-WPB77"
+        options={{
+          center: { lat: 37.8079, lng: 112.2690 },
+          zoom: 5,
+          pitch: 30,
+          baseMap: { type: 'vector', features: ['base', 'building3d'] },
+        }}
+      >
+        <${name}
+          ref={layerRef}
+        />
+      </BaseMap>
+    </div>
+  );
+};
+`;
